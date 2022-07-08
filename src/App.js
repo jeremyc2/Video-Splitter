bb6f658bfc965adfc31f364004327c1ead738744
Jeremy Chandler <jeremychandler94@gmail.com>
2022-07-08 10:08:58 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ function App() {
 
     await ffmpeg.load();
     ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
-    await ffmpeg.run('-i', name, '-f', 'segment', '-segment_time', segmentTime.toString(), '-segment_start_number', '1', '-vcodec', 'copy', '-reset_timestamps', '1', `${nameWithoutExtension} Part %d${fileExtension}`);
+    await ffmpeg.run('-i', name, '-f', 'segment', '-segment_time', segmentTime.toString(), 
+      '-segment_start_number', '1', '-vcodec', 'copy', '-reset_timestamps', '1', 
+      `${nameWithoutExtension} Part %d${fileExtension}`);
     const wasmFiles = await ffmpeg.FS('readdir', '.');
 
     wasmFiles
@@ -62,7 +64,7 @@ function App() {
       <div className='watermark'>VS</div>
       <h1 className='mt-10 font-extrabold tracking-tight text-center text-7xl lg:text-9xl'>Video Splitter</h1>
       <form className='p-5 mx-auto mt-10 text-xl bg-gray-900 border border-black rounded-md w-fit'>
-        <div className='flex gap-2 justify-center'>
+        <div className='flex justify-center gap-2'>
           <input ref={minuteRef}
             className='p-2 leading-tight text-center text-gray-700 bg-gray-200 border rounded selection:bg-slate-400 border-slate-400 focus:outline-none focus:bg-white'
             type="text" defaultValue='01' maxLength={2} size={2} pattern="\d*" onFocus={selectInput} autoFocus
