9ddef8c8c72e0b6dc0376ac2e9e22aa51236beea
Jeremy Chandler <jeremychandler94@gmail.com>
2022-07-10 12:53:53 -0500

    add group hover, fix aspect ratio for tall videos

--- src/ResultVideo/ResultVideo.js
+++ src/ResultVideo/ResultVideo.js
@@ -3,20 +3,20 @@ import React, { useRef } from 'react';
 export default function ResultVideo({name, objectUrl, index}) {
     const videoRef = useRef();
 
-    function handleVideoMouseEnter(e) {
+    function handleVideoMouseEnter() {
         videoRef.current.play();
     }
 
-    function handleVideoMouseLeave(e) {
+    function handleVideoMouseLeave() {
         videoRef.current.pause();
         videoRef.current.currentTime = 0;
     }
     
     return (
     <a download={name} href={objectUrl} onMouseEnter={handleVideoMouseEnter} onMouseLeave={handleVideoMouseLeave} 
-        className='flex flex-col overflow-hidden rounded-sm'>
-        <video ref={videoRef} src={objectUrl} muted></video>
-        <div className='flex justify-center gap-1 py-2 font-medium text-gray-700 bg-gray-50 px-7'>
+        className='flex flex-col overflow-hidden bg-black rounded-sm group'>
+        <video ref={videoRef} src={objectUrl} className='max-h-36' muted></video>
+        <div className='flex justify-center gap-1 py-2 font-medium text-gray-700 group-hover:bg-gray-300 bg-gray-50 px-7'>
             <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
             </svg>
